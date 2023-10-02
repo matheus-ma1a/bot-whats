9f79b43352a69bb920151f53c269c012d36891f3
Matheus Maia <matheussantosmaia@gmail.com>
2023-10-02 17:10:29 -0300

    troca de link

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function start(client) {
   Enquanto aguardamos o sinal, aproveitem para realizar o cadastro na plataforma onde descobrimos esse *HACK DENTRO DA CASA* .
 
   📲 CLIQUE AQUI PARA SE REGISTRAR: 
-  bit.ly/Deposite30eGanhe60Reais
+  bit.ly/-Deposite30eGanhe60Reais
     `)
       primeiraMsg(tempoInicial, tempoFinal, numero)
     }, 120000 )
@@ -138,10 +138,10 @@ function start(client) {
 
 
   🎁 CADASTRE-SE E GANHE ATÉ 200% DE BÔNUS NESSA PLATAFORMA.
-  bit.ly/Deposite30eGanhe60Reais
+  bit.ly/-Deposite30eGanhe60Reais
 
   📱 Jogar Fortune Tiger 🐯
-  bit.ly/CorretoraPaganteAqui
+  bit.ly/-CorretoraPaganteAqui
 
   ⚠ SINAL SÓ FUNCIONA NA PLATAFORMA ACIMA!
 
@@ -172,7 +172,7 @@ function start(client) {
 
   AGUARDE A PRÓXIMA SESSÃO E ME ENVIEM OS SEUS GANHOS NO PRIVADO 💥🥇		 
 
-  📲 CLIQUE AQUI PARA CADASTRAR : bit.ly/Deposite30eGanhe60Reais
+  📲 CLIQUE AQUI PARA CADASTRAR : bit.ly/-Deposite30eGanhe60Reais
 
           `)
       presinal()
